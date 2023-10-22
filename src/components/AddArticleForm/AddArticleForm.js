3bd1528d6d6ccc420c87bba93e8f9e00a4f53506
otter00 <ivlevanastia2001@mail.ru>
2023-10-22 17:02:00 +0300

    set title and description from event target value

--- src/components/AddArticleForm/AddArticleForm.js
+++ src/components/AddArticleForm/AddArticleForm.js
@@ -1,34 +1,69 @@
+import { Component } from "react";
 import { CustomButton } from "../../UI/CustomButton/CustomButton";
 import "./AddArticleFormStyles.scss";
 import CloseIcon from "@mui/icons-material/Close";
 
-export const AddArticleForm = ({ handleHideAddForm }) => {
-  return (
-    <>
-      <form action="" className="addArticleForm">
-        <button className="closeAddFormBtn" onClick={handleHideAddForm}>
-          <CloseIcon />
-        </button>
-        <h2>Создание поста</h2>
-        <div>
-          <input
-            type="text"
-            name="articleTitle"
-            placeholder="Заголовок поста"
-          />
-        </div>
-        <div>
-          <textarea name="articleDescription" placeholder="Ваш текст" />
-        </div>
-        <div>
-          <CustomButton
-            onClick={handleHideAddForm}
-            className={"addArticlesButton"}
-            name={"Опубликовать пост"}
-          />
-        </div>
-      </form>
-      <div className="formOverlay" onClick={handleHideAddForm}></div>
-    </>
-  );
-};
+export class AddArticleForm extends Component {
+  state = {
+    articleTitle: "",
+    articleDescription: "",
+  };
+
+  onChangeTitle = (event) => {
+    this.setState({
+      articleTitle: event.target.value,
+    });
+    // console.log(event.target.value);
+  };
+
+  onChangeDescription = (event) => {
+    this.setState({
+      articleDescription: event.target.value,
+    });
+    // console.log(event.target.value);
+  };
+
+  render() {
+    return (
+      <>
+        <form action="" className="addArticleForm">
+          <button
+            className="closeAddFormBtn"
+            onClick={this.props.handleHideAddForm}
+          >
+            <CloseIcon />
+          </button>
+          <h2>Создание поста</h2>
+          <div>
+            <input
+              type="text"
+              name="articleTitle"
+              placeholder="Заголовок поста"
+              value={this.state.articleTitle}
+              onChange={this.onChangeTitle}
+            />
+          </div>
+          <div>
+            <textarea
+              name="articleDescription"
+              placeholder="Ваш текст"
+              value={this.state.articleDescription}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div>
+            <CustomButton
+              onClick={this.props.handleHideAddForm}
+              className={"addArticlesButton"}
+              name={"Опубликовать пост"}
+            />
+          </div>
+        </form>
+        <div
+          className="formOverlay"
+          onClick={this.props.handleHideAddForm}
+        ></div>
+      </>
+    );
+  }
+}
