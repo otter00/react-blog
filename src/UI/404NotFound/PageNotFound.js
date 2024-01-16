bdd174e7a8e40196449eda5e42d5b3b111126126
otter00 <ivlevanastia2001@mail.ru>
2024-01-16 18:58:48 +0700

    navigation to blog page after 404 page reload

--- src/UI/404NotFound/PageNotFound.js
+++ src/UI/404NotFound/PageNotFound.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./PageNotFound.scss";
 import { useLocation, useNavigate } from "react-router-dom";
 import { CustomButton } from "../CustomButton/CustomButton";
 
 export const PageNotFound = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   // адрес пути, на котором находимся
   console.log(location);
-  let navigate = useNavigate();
+
+  useEffect(() => {
+    const checkNavigationType = () => {
+      // Проверяем тип навигации
+      if (performance && performance.getEntriesByType) {
+        const entries = performance.getEntriesByType("navigation");
+        console.log(entries);
+        if (entries.length > 0) {
+          const navigationType = entries[0].type;
+          if (navigationType === "reload") {
+            // Если тип навигации - reload, перенаправляем на /blog
+            navigate("/blog");
+          }
+        }
+      }
+    };
+
+    // Вызываем функцию проверки типа навигации
+    checkNavigationType();
+  }, [navigate]);
 
   const backToBlog = () => {
     navigate("/blog");
@@ -31,4 +51,13 @@ export const PageNotFound = () => {
   );
 };
 
-// if (!location?.state?.from?.pathname) return <Navigate to="/blog" />;
+/* deprecated but works pretty correctly (???) */
+// useEffect(() => {
+//   // Проверяем, была ли страница перезагружена
+//   if (window.performance) {
+//     if (performance.navigation.type === 1) {
+//       // Если страница была перезагружена, перенаправляем на /blog
+//       navigate("/blog");
+//     }
+//   }
+// }, [navigate]);
